e04938c416e8c7edf3ad44ad1f49b7839dc3830e
tm-git1 <westcoast.coders@mail.com>
2026-05-21 14:38:46 -0700

    Update ll_color.bat

--- ll_color.bat
+++ ll_color.bat
@@ -9,8 +9,8 @@ set "_="
 :_while
 if %_i% leq %_cnt% (
     ::echo %_i%
-    set /a _num=%_cnt%/1000000+E
-    color 1%_num% 
+    set /a _num=%_cnt%/1000000+6
+    color D%_num%                 ::e.g. color D7, try different things within range
     set /a _i=_i+1
     set "!_=!_!!=--!i"  
     cd /
