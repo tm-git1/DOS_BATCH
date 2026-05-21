263a3f18feb3461d2b55596b0a9d518683d43313
tm-git1 <westcoast.coders@mail.com>
2026-05-21 08:03:00 -0700

    Add files via upload

--- ll.bat
+++ ll.bat
@@ -1,4 +1,4 @@
-::Copyright (C) Thomas McCammon 2026
+::Copyright (C) Thomas McCammon
 @echo off
 setlocal EnableDelayedExpansion
 
@@ -9,10 +9,12 @@ set "_="
 :_while
 if %_i% leq %_cnt% (
     ::echo %_i%
+    set /a _num=%_cnt%/1000000+E
+    color 1%_num% 
     set /a _i=_i+1
-    set "!_=!_!!=--!"  REM toggle string expansion
-    cd / 
-    goto :_while
+    set "!_=!_!!=--!i"  
+    cd /
+    goto :_while   
 )
 
 EOF
