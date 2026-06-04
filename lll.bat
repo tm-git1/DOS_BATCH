01adf60521fdfb7839cc127de5e76c44be61de20
tm-git1 <westcoast.coders@mail.com>
2026-06-04 07:34:44 -0700

    Add files via upload

---
+++ lll.bat
@@ -0,0 +1,21 @@
+::Copyright (C) Thomas McCammon
+@echo off
+setlocal EnableDelayedExpansion
+
+set /a _i=1
+set /a _cnt=1000000
+set "_="
+
+:_while
+if %_i% leq %_cnt% (
+    ::echo %_i%
+    set /a _num=%_cnt%/1000000-1
+    color %_num%b 
+    set /a _i=_i+1
+    set "!_=!_!!=--!"-  
+    cd /
+    powershell /c .\ls.ps1 exit
+    goto :_while   
+)
+
+EOF
