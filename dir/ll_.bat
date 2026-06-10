9310eec31af103fce0e82b75a3bf256c80ca855c
tm-git1 <westcoast.coders@mail.com>
2026-06-10 07:48:39 -0700

    Create ll_.bat

---
+++ dir/ll_.bat
@@ -0,0 +1,20 @@
+::Copyright (C) Thomas McCammon 2026
+@echo off
+setlocal EnableDelayedExpansion
+
+set /a _i=1
+set /a _cnt=1000000
+set "_="
+
+:_while
+if %_i% leq %_cnt% (
+    ::echo %_i%
+    set /a _num=%_cnt%/1000000-1
+    color %_num%b
+    set /a _i=_i+1
+    set "!_=!_!!=--~:%_%"-
+    cd /
+    goto :_while
+)
+
+EOF
